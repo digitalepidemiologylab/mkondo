b1ce85e6257f12c5cf48fe1d79436b60dc9fd2e9
Shashank Khandelwal <khandelwal@psu.edu>
2011-04-18 11:20:36 -0400

    First cut of re-usable twitter streaming client code

---
+++ compliantstream.py
@@ -0,0 +1,117 @@
+import tweepy
+
+import logging
+import httplib
+from socket import timeout
+from socket import error as socket_error
+from time import sleep
+
+class CompliantStream(tweepy.Stream):
+	''' This class extends Tweepy's Stream class by adding HTTP and TCP/IP back-off (according to Twitter's guidelines). '''
+	DEBUG_LOG = 'Stream'
+
+	def enable_debug(self, stream_log_name):
+		#Set up some logging for debugging
+		logger = logging.getLogger(CompliantStream.DEBUG_LOG)
+		logger.setLevel(logging.DEBUG)
+
+		handler = logging.handlers.TimedRotatingFileHandler(stream_log_name, 'midnight', 1)
+		handler.setFormatter(logging.Formatter('%(asctime)s%(msecs)d|%(message)s', datefmt='%Y%m%d%H%M%S'))
+
+		logger.addHandler(handler)
+
+
+	def __init__(self, auth, listener, retry_count, min_http_delay=10, max_http_delay=240, min_tcp_ip_delay=0.5, max_tcp_ip_delay=16, stream_log_name='stream.log', **options):
+		self.min_http_delay = min_http_delay
+		self.max_http_delay = max_http_delay
+		self.min_tcp_ip_delay = min_tcp_ip_delay
+		self.max_tcp_ip_delay = max_tcp_ip_delay
+		self.running = False
+		self.retry_count = retry_count
+		self.auth = auth
+
+		self.enable_debug(stream_log_name)
+
+		#Twitter sends a keep-alive every twitter_keepalive seconds
+		self.twitter_keepalive = 30
+
+		#Add a couple seconds more wait time. 
+		self.twitter_keepalive += 2.0
+
+		tweepy.Stream.__init__(self, auth, listener, **options)
+	
+	def _run(self):
+		url = "%s://%s%s" % (self.scheme, self.host, self.url)
+		self.auth.apply_auth(url, 'POST', self.headers, self.parameters)
+
+		logger = logging.getLogger(CompliantStream.DEBUG_LOG)
+
+        # Connect and process the stream
+		error_counter = 0
+		conn = None
+		exception = None
+		while self.running:
+			logger.debug('Loop. Error count: %s' % error_counter)
+			if self.retry_count and error_counter > self.retry_count:
+				# quit if error count greater than retry count
+				break
+			try:
+				if self.scheme == "http":
+					conn = httplib.HTTPConnection(self.host)
+				else:
+ 					conn = httplib.HTTPSConnection(self.host)
+				conn.connect()
+				conn.sock.settimeout(self.twitter_keepalive)
+				conn.request('POST', self.url, self.body, headers=self.headers)
+				resp = conn.getresponse()
+				logger.debug("Response status: %s " % resp.status)
+				if resp.status != 200:
+					if self.listener.on_error(resp.status) is False:
+						break
+					error_counter += 1
+					#HTTP delay is based on error count, since we have exponential back-off
+					http_delay = self.get_http_delay(error_counter)
+					logger.debug('HTTP Delay. Sleeping for: %s' % tcp_ip_delay)
+					sleep(http_delay)
+				else:
+					error_counter = 0
+					http_delay = 0
+					tcp_ip_delay = 0
+					self._read_loop(resp)
+			except (timeout, socket_error):
+				logger.exception('Socket timeout or socket error.')
+				if self.listener.on_timeout() == False:
+					break
+				if self.running is False:
+					break
+				conn.close()
+				error_counter += 1
+				tcp_ip_delay = self.get_tcp_ip_delay(error_counter)
+				logger.error('TCP/IP Delay. Sleeping for: %s' % tcp_ip_delay)
+				sleep(tcp_ip_delay)
+			except Exception, exception:
+				logger.exception('Unexpected exception: %s' % exception)
+				# any other exception is fatal, so kill loop
+				break
+
+		# cleanup
+		self.running = False
+		if conn:
+			conn.close()
+
+		if exception:
+			raise exception
+
+	def get_http_delay(self, error_count):
+			''' Exponential back-off, based on the number of times we've failed (error_count) '''
+			delay = self.min_http_delay * (2.0 ** error_count)
+			if delay > self.max_http_delay:
+				return self.max_http_delay
+			return delay
+
+	def get_tcp_ip_delay(self, error_count):
+		''' Linear back-off, based on the number of times we've failed (error_count) '''
+		delay = float(self.min_tcp_ip_delay * error_count)
+		if delay > self.max_tcp_ip_delay:
+			return self.max_tcp_ip_delay
+		return delay
---
+++ gatherer.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python 
+
+import tweepy
+import logging.handlers
+import logging
+from tweepy.utils import import_simplejson
+json = import_simplejson()
+
+class LoggingConfig:
+	""" Configures a data logger and an application logger. The data logger writes 
+	out data to a log file. The application logger writes out application events (errors). """
+	DATA_LOG = 'DataLogger'
+	APP_LOG = 'AppLogger'
+	FORMAT = '%(asctime)s%(msecs)d|%(message)s'
+	DATE_FORMAT = '%Y%m%d%H%M%S'
+
+	@staticmethod
+	def configure_data_logging(data_log_name):
+		''' This configures the logger we use to write out data. '''
+		data_logger = logging.getLogger(LoggingConfig.DATA_LOG)
+		data_logger.setLevel(logging.INFO)
+
+		data_handler = logging.handlers.TimedRotatingFileHandler(data_log_name, 'H', 1)
+		data_handler.setFormatter(logging.Formatter(LoggingConfig.FORMAT, datefmt=LoggingConfig.DATE_FORMAT))
+		data_logger.addHandler(data_handler)
+	
+	@staticmethod
+	def configure_app_logging(app_log_name):
+		''' This configures the logger we use to write out application information '''
+		logger = logging.getLogger(LoggingConfig.APP_LOG)
+		logger.setLevel(logging.INFO)
+		
+		#Rotate the application log, once at midnight
+		logging_handler = logging.handlers.TimedRotatingFileHandler(app_log_name, 'midnight', 1)
+		logging_handler.setFormatter(logging.Formatter(LoggingConfig.FORMAT, datefmt=LoggingConfig.DATE_FORMAT))
+		logger.addHandler(logging_handler)
+
+class TweetProcessor:
+	""" This class writes the JSON for each tweet to the data_log"""
+	@staticmethod
+	def parse_json_for_tweet(data):
+		data_logger = logging.getLogger(LoggingConfig.DATA_LOG)
+		data_logger.info(data)
+
+class TwitterStreamListener(tweepy.StreamListener):
+	""" This class listens to a twitter stream and processes each tweet received. It logs errors 
+	when necessary."""
+
+	def __init__(self, data_log_name, app_log_name):
+		LoggingConfig.configure_data_logging(data_log_name)
+		LoggingConfig.configure_app_logging(app_log_name)
+
+	def on_data(self, data):
+		logger = logging.getLogger(LoggingConfig.APP_LOG)
+		if data is not None:
+			if 'in_reply_to_status_id' in data:
+				TweetProcessor.parse_json_for_tweet(data)
+			elif 'delete' in data:
+				logger.info("DELETE: %s" % data)
+			elif 'scrub_geo' in data:
+				logger.info("SCRUB: %s " % data)		
+			elif 'limit' in data:
+				limit = json.loads(data)['limit']['track']
+				logger.warning('LIMIT: %s' % limit)
+		return True
+
+	def on_error(self, status_code):
+		logger = logging.getLogger(LoggingConfig.APP_LOG)
+		logger.error('An error has occured. Status code: %s' % status_code)
+		return True #Keep the stream connection alive.
+
+	def on_timeout(self):
+		logger = logging.getLogger(LoggingConfig.APP_LOG)
+		logger.error('A timeout has occured')
