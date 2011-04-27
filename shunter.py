9ef06eda27b94067d01b89039c7fed9bd75ca0ca
Shashank Khandelwal <khandelwal@psu.edu>
2011-04-27 13:30:27 -0400

    Code to shunt files into date based directories

---
+++ shunter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+""" This is the shunter. It shunts zipped data files from the twitter
+collection project into sub-directories for each day. 24 files/day adds up to a
+lot of files in one directory. To prevent that, each day has it's own
+directory. """
+
+import os
+import shutil
+
+def get_directory_file_list(datadir):
+	directory_listing = os.listdir(datadir)
+
+	#separate directories, and data files
+
+	directories = []
+	data_files = []
+	for d in directory_listing:
+		if os.path.isdir(d):
+			directories.append(d)
+		elif 'gz' in d:
+			#We're only interested in shunting the zipped files
+			data_files.append(d)
+
+	return (directories, data_files)
+
+def extract_dir_name(data_file_name):
+	""" From the data filename, extract the date """
+	#Example file name: tweets.txt.2011-03-14_04.gz
+	return data_file_name.split('.')[2].split('_')[0].replace('-', '')
+
+def extract_directory_names(data_files):
+	""" From the list of data_files, extract what the directory names should be. """
+	days = [extract_dir_name(d) for d in data_files if len(d) > 1]
+	#The day of the year is the name of the directory
+	return set(days)
+
+def create_directories(path_prefix, dir_names):
+	""" Create the directories if they do not already exist """
+	for d in dir_names:
+		path = os.path.join(path_prefix, d)
+		if not os.path.exists(path):
+			os.mkdir(path)
+
+def move_files(path_prefix, data_files):
+	for f in data_files:
+		directory = os.path.join(path_prefix, extract_dir_name(f))
+		src = os.path.join(path_prefix, f)
+		dest = os.path.join(directory, f)
+		shutil.move(src, dest)
+
+if __name__ == '__main__':
+	(directories, data_files) = get_directory_file_list(settings.DATA_DIRECTORY)
+	dir_names = extract_directory_names(data_files)
+	create_directories(settings.DATA_DIRECTORY, dir_names)
+	move_files(settings.DATA_DIRECTORY, data_files)
