3643342e9bec2c5b00c2088c3c910a2c3bab5e04
Shashank Khandelwal <shrew@alumni.cs.utexas.edu>
2012-02-23 14:47:53 -0500

    Setting up mkondo with setuptools so that we can distribute better

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+	name = "Mkondo",
+	version = "0.0.1",
+	description = "Mkondo is a library that helps download Twitter Streaming API data and manage that data.",
+	long_description = " Contains code that complies with the Streaming API T&C. Contains code to backup that data, shunt it around, and finally, analyze portions of it", 
+	author = "Shashank Khandelwal", 
+	author_email = "shrew@alumni.cs.utexas.edu", 
+	url="http://salathegroup.com", 
+	packages = find_packages(exclude="test"),
+	zip_safe = False
+)
