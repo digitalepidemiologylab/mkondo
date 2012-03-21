a47f11507743beb6b6b7a6b60abd8b702e680dd0
Shashank Khandelwal <shrew@alumni.cs.utexas.edu>
2012-03-21 16:40:30 -0400

    Changed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "mkondo",
-	version = "0.0.2",
+	version = "0.0.3",
 	description = "Mkondo is a library that helps download Twitter Streaming API data and manage that data.",
 	long_description = " Contains code that complies with the Streaming API T&C. Contains code to backup that data, shunt it around, and finally, analyze portions of it", 
 	author = "Shashank Khandelwal", 
